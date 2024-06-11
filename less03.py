f1e150900d45436c577017e46ea2cd9d3f699ac2
igorTopko <igor.topko@icloud.com>
2024-06-11 23:53:14 +0300

    Homework2

--- less03.py
+++ less03.py
@@ -1,21 +1,16 @@
-value = input("Введіть операцію (+, -, *, /): ")
+value = input('Введіть операцію (+, -, *, /): ')
 
-num1 = int(input("Введіть перше число: "))
-num2 = int(input("Введіть друге число: "))
+num1 = int(input('Введіть перше число: '))
+num2 = int(input('Введіть друге число: '))
 
-if num2 == 0:
-    print('Помилка: Ділення на нуль неможливе')
-elif (value == '+'):
+if value == '+':
     print(num1 + num2)
 elif value == '-':
     print(num1 - num2)
 elif value == '*':
     print(num1 * num2)
 elif value == '/':
-    print(num1 / num2)
-else:
-    result == num1 / num2
-    print ('result')
-
-
-
+    if num2 == 0:
+        print('Помилка: Неправильна операція')
+    else:
+        print('Не можливо ділення на нуль')

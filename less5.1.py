1ff681908eb82258cf8f1f076174d838d166fe48
igorTopko <igor.topko@icloud.com>
2024-06-21 21:17:37 +0300

    Homework2

--- less5.1.py
+++ less5.1.py
@@ -1,17 +1,1 @@
-value_one = "i Love Python"
-value_two = "Thank You So Much"
-value_three = "This Is Really Cool"
-
-hashtag_one = "#" + "".join(value_one.split())
-hashtag_two = "#" + "".join(value_two.split())
-hashtag_three = "#" + "".join(value_three.split())
-
-if len(hashtag_one) > 140:
-    hashtag_one = hashtag_one[:140]
-elif len(hashtag_two) > 140:
-    hashtag_two = hashtag_two[:140]
-elif len(hashtag_three) > 140:
-    hashtag_three = hashtag_three[:140]
-print(hashtag_one)
-print(hashtag_two)
-print(hashtag_three)
+print ('hello')

3caffedf93926d7e11d8db7c0ef94a34d9dafd21
igorTopko <igor.topko@icloud.com>
2024-06-30 21:30:03 +0300

    Homework2

--- less7.1.py
+++ less7.1.py
@@ -1,5 +1,6 @@
-name = input("Введіть ваше ім'я: ")
-age = int(input("Введіть ваш вік: "))
+def say_hi(name, age):
+    return f"Hi. My name is {name} and I'm {age} years old"
 
-result = f"Привіт, мене звуть {name} і мені {age} років."
-print(result)
+assert say_hi("Igor", 27) == "Hi. My name is Igor and I'm 27 years old", 'Test1'
+assert say_hi("Mark", 32) == "Hi. My name is Mark and I'm 32 years old", 'Test2'
+print("OK")

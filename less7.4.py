ae3ac794d23750e02283f5fd5005ef0ce8bae55f
igorTopko <igor.topko@icloud.com>
2024-07-01 19:34:15 +0300

    Homework2

--- less7.4.py
+++ less7.4.py
@@ -1,5 +1,5 @@
 def common_elements():
-    return set(num for num in range(1, 100) if num % 3 == 0) & set(num for num in range(1, 100) if num % 5 == 0)
+    return set(num for num in range(0, 100) if num % 3 == 0) & set (num for num in range(0, 100) if num % 5 == 0)
 result = common_elements()
 print(result)
 assert common_elements()
